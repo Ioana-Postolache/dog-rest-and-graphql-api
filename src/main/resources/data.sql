c183e3bc5b85df3a25d1960406e88068afcd62b3
postolac <postolac@adobe.com>
2021-04-26 19:35:20 +0300

    Initial commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO dog (id, name, breed, origin) VALUES (1, 'Doggy','Afghan', 'Afghanistan');
+INSERT INTO dog (id, name, breed, origin) VALUES (2, 'Spotty','Dalmatian', 'Croatia');
+INSERT INTO dog (id, name, breed, origin) VALUES (3, 'King','Greyhound', 'Egypt');
